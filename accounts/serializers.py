d4adbee8b34c75616ca96d0ef169364f6e4b24b8
vahid1378518 <vahid1378518@gmail.com>
2024-08-12 14:27:12 +0330

    Add migrations, Complete UserSerializer and UserRegistration view (Part 1).

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,4 +4,24 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username',
+                  'password',
+                  'phone',
+                  'address',
+                  'gender',
+                  'age',
+                  'description',
+                  'first_name',
+                  'last_name',
+                  'email')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import CreateAPIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
@@ -19,4 +20,10 @@ class LogoutAPIView(APIView):
 
 
 class UserRegistration(APIView):
-    pass
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
