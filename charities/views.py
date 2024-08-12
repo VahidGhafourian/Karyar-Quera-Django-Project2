541ce9b9005e3c2b5b4ed005aa7c622c83ec81a8
vahid1378518 <vahid1378518@gmail.com>
2024-08-12 15:03:07 +0330

    Complete TaskRequest view. (Part 3)

--- charities/views.py
+++ charities/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from accounts.permissions import IsCharityOwner, IsBenefactor
-from charities.models import Task
+from charities.models import Task, Benefactor
 from charities.serializers import (
     TaskSerializer, CharitySerializer, BenefactorSerializer
 )
@@ -73,7 +73,15 @@ class Tasks(generics.ListCreateAPIView):
 
 
 class TaskRequest(APIView):
-    pass
+    permission_classes = [IsAuthenticated, IsBenefactor]
+
+    def get(self, request, task_id):
+        task = get_object_or_404(Task, id=task_id)
+        if task.state != Task.TaskStatus.PENDING:
+            return Response(data={'detail': 'This task is not pending.'}, status=status.HTTP_404_NOT_FOUND)
+        task.state = Task.TaskStatus.WAITING
+        task.assign_to_benefactor(Benefactor.objects.get(user=request.user))
+        return Response(data={'detail': 'Request sent.'}, status=status.HTTP_200_OK)
 
 
 class TaskResponse(APIView):
